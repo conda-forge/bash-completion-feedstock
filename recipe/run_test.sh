026359fcd14a002de19790b82df4e64e1b71fad1
Matthias Diener <mdiener@illinois.edu>
2022-10-28 19:51:10 -0500

    another try

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -8,8 +8,10 @@ if [ ${BASH_VERSINFO[0]} -gt 4 ] || \
     [ ${BASH_VERSINFO[0]} -eq 4 -a ${BASH_VERSINFO[1]} -ge 1 ]; then
 
     # Remove stray completion file in docker image
-    alias _yum='true'
+    echo 'true' > $PREFIX/bin/_yum
 
     # Ensure the library loads without error at least
     bash -e bash_completion
+
+    rm $PREFIX/bin/_yum
 fi
